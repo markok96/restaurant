b14a4ec5d3c81846d70949397fc808d0cd2b8704
Marko Korolija <mkorolija3@gmail.com>
2021-05-26 15:55:03 +0200

    mobile menu, nav update

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 const calendar = document.querySelector('.cal-input');
 
 datepicker(calendar);
+
+const menuBtn = document.querySelector('.menu-btn');
+
+const menu = document.querySelector('.menu');
+
+menuBtn.addEventListener('click', () => {
+  menu.classList.toggle('toggle-list');
+  menuBtn.classList.toggle('toggle-bar');
+})
